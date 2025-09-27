58fda9ab1031d4a2cfc8127fe3ccb79b27ab475c
nullbishwa <bishwaroop2005@gmail.com>
2025-09-27 20:55:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: "./",   // root directory where index.html is located
+  build: {
+    outDir: "dist",
+  },
 });
