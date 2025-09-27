82d71df2cc69f0bd36852034812514384bc4675f
nullbishwa <bishwaroop2005@gmail.com>
2025-09-27 20:52:26 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./blog-frontend/index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
